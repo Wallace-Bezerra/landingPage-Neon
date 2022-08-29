e82074c5ca3e60b47b026555ea9aad95e8eb6719
Wallace-Bezerra <89711908+Wallace-Bezerra@users.noreply.github.com>
2022-08-29 20:18:25 -0300

    menu-mobile

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const logo = document.querySelector("header .logo img");
 
 function mudarMenu(event) {
   console.log(event.currentTarget);
-  if (scrollY > 0) {
+  if (scrollY > 80) {
     botaoMenu.classList.remove('white');
     menu.classList.add('fixed');
     logo.src = "./img/logo-neon-color.svg";
@@ -23,7 +23,7 @@ window.addEventListener('scroll', mudarMenu)
 const produto = document.querySelector("[href='#produtos']");
 const dropdown = document.querySelector(".dropdown");
 
-
+//dripdown
 produto.addEventListener('click', activeDropdown);
 dropdown.addEventListener('mouseleave', activeDropdown);
 function activeDropdown(event) {
@@ -39,3 +39,27 @@ function activeDropdown(event) {
     }, 200);
   }
 }
+
+//menu-mobile
+
+const menuMobile = document.querySelector(".btn-mobile");
+const overlayMobile = document.querySelector(".overlay-mobile");
+
+
+menuMobile.addEventListener("click", ativarOverlay);
+
+function ativarOverlay() {
+  overlayMobile.classList.toggle("ativo");
+  console.log("wallace");
+}
+
+overlayMobile.addEventListener('click', (event) => {
+  if (event.target === event.currentTarget) {
+    overlayMobile.classList.remove("ativo");
+  }
+});
+
+// function ativarOverlay2() {
+//   overlayMobile.classList.toggle("ativo");
+//   console.log("overr2");
+// }
